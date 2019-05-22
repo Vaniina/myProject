39d78d2ed8a83875567e40d6c3d8802cbdf625a7
Vaniina <anna.vessereau@gmail.com>
2019-05-22 22:32:17 +0200

    add firebase rest api

--- typescript/functions/src/index.ts
+++ typescript/functions/src/index.ts
@@ -1,11 +1,14 @@
 import * as functions from 'firebase-functions';
-import * as cors from "cors";
-
-import express = require('express');
+import * as admin from 'firebase-admin';
+import * as bodyParser from 'body-parser';
+import * as express from 'express';
 import list from './hostels';
 
+
+admin.initializeApp();
+
 const app = express();
-app.use(cors({ origin: true }))
+app.use(bodyParser.json());
 
 
 app.get('/list', (req, res) => {
@@ -27,11 +30,11 @@ app.get('/:id', (req, res) => {
     } else {
         res.status(404);
         res.send({
-            status: "error"
+            status: "error",
+            error: "Hostel not found"
         });
     }
 });
-
 
 app.delete('/:id', (req, res) => {
     const hostelId = parseInt(req.params.id, 10);
@@ -45,6 +48,41 @@ app.delete('/:id', (req, res) => {
         status: "success"
     });
 });
+
+app.post('/list', (req, res) => {
+    const newHostel = req.body;
+
+    newHostel.id = 'test';
+    newHostel.roomNumbers = 0;
+    newHostel.rooms = [];
+    list.push(newHostel);
+
+    res.send({
+        status: "success",
+        data: list
+    });
+});
+
+app.put('/:id', (req, res) => {
+    const hostelId = parseInt(req.params.id, 10);
+    const hostel = list.find(hostel => hostel.id === hostelId);
+
+    if (hostel) {
+        hostel.name = req.body.name;
+        res.send({
+            status: "success",
+            data: hostel
+        });
+    } else {
+        res.send({
+            status: "error",
+            error:  "not found"
+        });
+    }
+});
+
+
+
 
 export const hostels = functions.https.onRequest(app);
 
